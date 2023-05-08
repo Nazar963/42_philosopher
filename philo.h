32e0fba35c6f7d62c64466dd8804f2345b7e775f
Nazar <nizarbonex1@gmail.com>
2023-05-08 17:15:30 +0200

    fin

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: naal-jen <naal-jen@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 15:51:14 by naal-jen          #+#    #+#             */
-/*   Updated: 2023/05/08 12:50:38 by naal-jen         ###   ########.fr       */
+/*   Updated: 2023/05/08 17:15:12 by naal-jen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,19 @@ typedef struct s_loco
 	t_philo			*p;
 }		t_loco;
 
+/* ------------------------------- get_time.c ------------------------------- */
+long long	fetch_time(void);
+void		ft_usleep(int time);
+
+/* --------------------------------- main.c --------------------------------- */
+void		free_all_one(char **av, t_loco *loco);
+void		free_all_plus(pthread_t com, char **av, t_loco *loco);
+pthread_t	go_for_many(char **av, t_loco *loco);
+
+/* ----------------------------- philos_helper.c ---------------------------- */
+int			camm_helper(int j, t_loco *loco);
+void		*camm(void	*arg);
+
 /* --------------------------------- utils.c -------------------------------- */
 long		ft_atoi(const char *nptr);
 long		ft_sp_atoi(int ac, char **av);
@@ -61,17 +74,14 @@ int			ft_isdigit_new(char *str);
 /* ------------------------------- validate.c ------------------------------- */
 int			check_args(int ac, char **av);
 int			check_values(t_loco *loco);
+t_loco		*initialize_helper_1(char **av, t_loco *loco);
+t_loco		*initialize_philos(int ac, char **av, int i, t_loco *loco);
 t_loco		*initialize(int ac, char **av, t_loco *loco);
 
 /* ------------------------------- one_philo.c ------------------------------ */
 void		*one(void *arg);
 
-/* ------------------------------- get_time.c ------------------------------- */
-long long	fetch_time(void);
-void		ft_usleep(int time);
-
 /* --------------------------------- print.c -------------------------------- */
-void		*camm(void	*arg);
 void		print_fork(t_philo *philo);
 void		print_eating(t_philo *philo);
 void		print_thinking(t_philo *philo);
